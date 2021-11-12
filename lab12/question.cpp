b8807f9cdc1cfbe9c4bdc1e035295edcde8e7ed8
priyanshu-rajput-01 <priyanshurajput0071109@gmail.com>
2021-11-12 13:51:55 +0530

    Added lab13

---
+++ lab12/question.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class Cars{
+    private:
+        string model;
+        float cost;
+        long units;
+    public :
+        Cars(int len);
+        void display();
+};
+Cars::Cars(int len){
+    
+}
+void Cars::display(){
+
+}
+
+int main(){
+
+}
