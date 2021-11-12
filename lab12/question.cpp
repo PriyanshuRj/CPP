3f9ec2d1bf04ed6809e5676bb7ec262eee70e4a2
priyanshu-rajput-01 <priyanshurajput0071109@gmail.com>
2021-11-12 16:17:34 +0530

    changed

--- lab12/question.cpp
+++ lab12/question.cpp
@@ -1,21 +1,54 @@
 #include<iostream>
+#include<map>
 using namespace std;
-class Cars{
-    private:
-        string model;
-        float cost;
-        long units;
-    public :
-        Cars(int len);
-        void display();
-};
-Cars::Cars(int len){
-    
-}
-void Cars::display(){
 
-}
 
 int main(){
+    map<pair<string, int>, int> cars;
+    int number = 0;
+    cout<<"Enter the No. of cars you want to add in the table : ";
+    cin>>number;
+    for(int i=0;i<number;i++){
+        string model = "";
+        int cost,unit_sold;
+        cout<<"Enter the model for the "<<i+1<<"th car :";
+        cin>>model;
+        cout<<"Enter the cost of the model : ";
+        cin>>cost;
+        cout<<"Enter the No. of units sold : ";
+        cin>>unit_sold;
+        cars[pair(model, cost)] = unit_sold;
 
+    }
+    cout<<endl;
+    cout<<"\n\n\t\tIntractive Display ::::::::::\n ";
+    int ch =0;
+    do{
+        cout<<"\nPress 1 for Displaying the details for a particular model\nelse to exit\n\n";
+        cout<<"Enter choice : ";
+        cin>>ch;
+        if(ch==1){
+            cout<<"Enter the name for the model you want to display details of\n";
+            string model;
+            cin>>model;
+            int cost;
+            cout<<"Enter the cost of the model : ";
+            cin>>cost;
+            if(!cars.count(pair(model,cost))){
+                cout<<"Sorry you entered wrong model name\n";
+            }
+            else{
+                cout<<"The no. of cars sold for model "<<model<<" : "<<cars[pair(model,cost)]<<endl;
+                cout<<"The total cost is : "<<cost*cars[pair(model,cost)]<<endl;
+
+            }
+
+
+        }
+        else{
+            cout<<"Thanks for visiting !!"<<endl;
+            break;
+        }
+    }while(ch!=1);
+    return 0;
 }
