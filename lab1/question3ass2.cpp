c2a9918324566d690832c3e094b686de541e0f55
priyanshu-rajput-01 <priyanshurajput0071109@gmail.com>
2021-11-30 17:21:21 +0530

    Added code

--- lab1/question3ass2.cpp
+++ lab1/question3ass2.cpp
@@ -5,12 +5,12 @@ int main(){
     int n,m;
     cout<<"Enter the dimensions of the matrix (seprated by a space):";
     cin>>n>>m;
-    int arr1[n][m] ,max[m];
+    int arregular_typist[n][m] ,max[m];
     string arr2[n][m];
     cout<<endl<<"Enter the element of the first array :"<<endl<<endl;
     for(int i=0;i<n;i++) for(int j =0;j<m;j++){
         cout<<"Enter the element at row no. "<<i+1<<" and column no. "<<j+1<<" : ";
-        cin>>arr1[i][j];
+        cin>>arregular_typist[i][j];
 
     }
     cout<<endl<<endl<<"Enter the element of the second array :"<<endl<<endl;
@@ -20,7 +20,7 @@ int main(){
             cout<<"Enter the element at row no. "<<i+1<<" and column no. "<<j+1<<" : ";
             int inp2;
             cin>>inp2;
-            arr2[i][j] = to_string(inp2+arr1[i][j]);
+            arr2[i][j] = to_string(inp2+arregular_typist[i][j]);
             
 
 
