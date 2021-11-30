b4035b3dca028f44a3c6cbb82b9733466daf9272
priyanshu-rajput-01 <priyanshurajput0071109@gmail.com>
2021-11-30 20:21:12 +0530

    Added files

---
+++ Practice/classes.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    scanf("%d",&t);
+    for(int it =0;it<t;it++){
+        printf("Case %d: ",it+1);
+    }
+    return 0;
+}
