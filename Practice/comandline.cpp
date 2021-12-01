73c37fab90668bc6dc584bd584acf049895b1505
priyanshu-rajput-01 <priyanshurajput0071109@gmail.com>
2021-12-01 14:37:52 +0530

    Added new files

---
+++ Practice/comandline.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(int argc, char** argv){
+    if(argc<2){
+        cout<<"Usage: "<<argv[0]<<" <filename>\n";
+        return 0;
+    }
+    else{
+        cout<<argv[1]<<"This is the first argument"<<endl;
+    }
+    
+    return 0;
+}
