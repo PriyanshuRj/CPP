d2332f246662a0594d0d5c59775e2507c3e6e385
PriyanshuRj <priyanshurajput0071109@gmail.com>
2022-04-19 10:52:24 +0530

    Added lan2

---
+++ SEM4/lab2/bubblesort.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(int v[], int n)
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+    {
+        bool flag = true;
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (v[j] > v[j + 1])
+            {
+                swap(v[j], v[j + 1]);
+                flag = false;
+            }
+        }
+        if (flag)
+            break;
+    }
+}
+
+int main()
+{
+    cout << "Input Number of elements : ";
+    int n;
+    cin >> n;
+    int *v = new int[n];
+    cout << "Input all elements :::::" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter the element at index " << i << " : ";
+        cin >> v[i];
+    }
+    bubbleSort(v, n);
+    cout << "List after Sorting :" << '\n';
+    for (int i = 0; i < n; i++)
+        cout << v[i] << " ";
+    cout<<endl;
+    return 0;
+}
